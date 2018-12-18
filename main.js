d9534cbe24bbeab9369d4b9e2d582d433a4a0753
Kennedy <35559656+mbogokennedy@users.noreply.github.com>
2018-12-18 04:17:02 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 // Initialize Firebase
 var config = {
-  apiKey: "AIzaSyBirmO17HD1FY4VxMWT4qnLFBuqlAOz2ko",
-  authDomain: "cleanerwebs-3a394.firebaseapp.com",
-  databaseURL: "https://cleanerwebs-3a394.firebaseio.com",
-  projectId: "cleanerwebs-3a394",
-  storageBucket: "cleanerwebs-3a394.appspot.com",
-  messagingSenderId: "680908881758"
+  apiKey: "XXXXXXXXX",
+  authDomain: "XXXXXXXXXX",
+  databaseURL: "XXXXXXXXXX",
+  projectId: "XXXXXXXXXX",
+  storageBucket: "XXXXXXXXXX",
+  messagingSenderId: "XXXXXXXXXX"
 };
 firebase.initializeApp(config);
 // Reference messages collection
